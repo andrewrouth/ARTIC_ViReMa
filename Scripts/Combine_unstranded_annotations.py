0c26f929af31a99e06d6c6ff124ee9fe683d2b5e
andrewrouth <123663561+andrewrouth@users.noreply.github.com>
2024-12-18 15:12:55 -0800

    Add files via upload

--- Scripts/Combine_unstranded_annotations.py
+++ Scripts/Combine_unstranded_annotations.py
@@ -2,32 +2,19 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("InBED", help= "Input BED File")
 parser.add_argument("OutBED", help= "Root of output files")
-parser.add_argument("-BED12", action='store_true', help= "Select ViReMa output was in BED12")
-parser.add_argument("-Stranded", action='store_true', help= "Select if coverage from ViReMa output was in BED12 files and as '-Stranded'")
+parser.add_argument("-CountCombine",  action='store_true', help= "")
 args = parser.parse_args()
 
-if args.Stranded:
-    Stranded = True
+if args.CountCombine:
+    CountCombine = True
 else:
-    Stranded = False
-if args.BED12:
-    BED12 = True
-else:
-    BED12 = False
+    CountCombine = False
 
 class RecEvent(object):
     def __init__(self, line, Name):
-        if BED12: ###Just realized this is actually BED10.....Need to revisit ViReMa
-            [self.Ref, self.Start, self.Stop, 
-             self.Type, self.Count, self.Dir, 
-             self.CSL, self.CSR, self.Seq1, self.Seq2] = line
-        else:
-            [self.Ref, self.Start, self.Stop, 
-             self.Type, self.Count, self.Dir] = line
-        if self.Dir == '-':
-            self.Start, self.Stop = self.Stop, self.Start
-        else:
-            pass
+        [self.Ref, self.Start, self.Stop, 
+         self.Type, self.Count, self.Dir, 
+         self.CSL, self.CSR, self.Seq1, self.Seq2] = line
         self.Name = Name
         self.Count = int(self.Count)
         self.CSL = int(self.CSL)
@@ -42,31 +29,31 @@ with open(str(args.InBED),'r') as In:
     Out.write(Header)
     Data = In.readline().rstrip().split()
     while Data:
-        if Data[5] == '+':
-            Name = Data[0] + ":" + Data[1] +  "_to_" + Data[2]
+        #print(Data)
+        if Data[0] == 'track':
+            print("New Header in middle of file: ", Data)
         else:
-            Name = Data[0] + ":" + Data[2] +  "_to_" + Data[1]
-        if Name in Dict:
-            Dict[Name].Count += int(Data[4])
-            if BED12:
-                if Stranded:    #If -Stranded, then Coverage was seperately reported for each stranded
+            if Data[5] == '-':
+                Data[2],Data[1] = Data[1],Data[2]
+                #Name = Data[0] + ":" + Data[1] +  "_to_" + Data[2]
+            else:
+                #Name = Data[0] + ":" + Data[2] +  "_to_" + Data[1]
+                pass
+            Name = Data[0] + ":" + Data[1] +  "_to_" + Data[2]
+            if Name in Dict:
+                Dict[Name].Count += int(Data[4])
+                if CountCombine:
                     Dict[Name].CSL += int(Data[6])
                     Dict[Name].CSR += int(Data[7])
-                else:           #If not -Stranded, then Coverage will be same for both and already combined
+                else:
                     pass
             else:
-                pass
-        else:
-            Dict[Name] = RecEvent(Data, Name)
+                Dict[Name] = RecEvent(Data, Name)
         Data = In.readline().rstrip().split()
         
 for i in Dict:
-    if BED12:
-        line = '\t'.join([Dict[i].Ref, Dict[i].Start, Dict[i].Stop, 
-                          Dict[i].Type, str(Dict[i].Count), '+/-', 
-                          str(Dict[i].CSL), str(Dict[i].CSR), Dict[i].Seq1, Dict[i].Seq2])
-    else:
-        line = '\t'.join([Dict[i].Ref, Dict[i].Start, Dict[i].Stop, 
-                          Dict[i].Type, str(Dict[i].Count), '+/-',])
+    line = '\t'.join([Dict[i].Ref, Dict[i].Start, Dict[i].Stop, 
+                      Dict[i].Type, str(Dict[i].Count), '+/-', 
+                      str(Dict[i].CSL), str(Dict[i].CSR), Dict[i].Seq1, Dict[i].Seq2])
     Out.write(line + '\n')
 Out.close()
